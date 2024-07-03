f86137ca717d5d1f8f6aacf5126d925ba6ddd264
onyyyyy <hongki365@gmail.com>
2024-07-04 01:35:31 +0900

    메인 배너 수정

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -7,8 +7,13 @@ import { productData } from "../../data/productData";
 import { Link } from "react-router-dom";
 import { EffectCoverflow, Pagination } from "swiper/modules";
 
+const BannerWrap = styled.div`
+  width: 100%;
+  padding: 5% 0;
+`;
+
 const MainBnner = styled.div`
-  width: 500px;
+  width: 100%;
   height: 500px;
   background: url(${(props) => props.$bgUrl}) no-repeat center / cover;
 `;
@@ -17,22 +22,39 @@ const params = {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 4,
+  slidesPerView: 3,
   coverflowEffect: {
     rotate: 60,
     stretch: 0,
     depth: 100,
     modifier: 1,
-    slideShadows: true,
+    slideShadows: false,
   },
 
   modules: [EffectCoverflow, Pagination],
 };
 
-const BannerWrap = styled.div`
-  width: 100%;
-  height: 80vh;
-  padding-top: 5%;
+const PName = styled.h4`
+  font-size: 26px;
+  margin: 20px 0 10px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PPrice = styled.h5`
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PDecs = styled.p`
+  font-size: 16px;
+  margin-bottom: 20px;
+  color: #808080;
+  display: flex;
+  justify-content: center;
 `;
 export const Main = () => {
   return (
@@ -43,6 +65,9 @@ export const Main = () => {
             <SwiperSlide key={data.id}>
               <Link to={`/detail/`} state={{ pData: data }}>
                 <MainBnner $bgUrl={data.pImg[0].imgUrl} />
+                <PName>{data.pName}</PName>
+                <PDecs>{data.pDesc}</PDecs>
+                <PPrice>{data.pPrice}</PPrice>
               </Link>
             </SwiperSlide>
           ))
